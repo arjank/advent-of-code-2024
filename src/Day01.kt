d5df5615ec088c9bd16265068f9429fdc71d7169
Arjan Kleene <contact@arjankleene.com>
2024-12-07 17:11:04 +0100

    My solution for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(left: List<Int>, right: List<Int>): Int {
+        return left.sorted()
+            .zip(right.sorted())
+            .map { (left, right) -> abs(left - right) }
+            .reduce { a, b -> a + b }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(left: List<Int>, right: List<Int>): Int {
+        val frequencies = right.groupingBy { it }.eachCount()
+
+        return left
+            .map { frequencies.getOrDefault(it, 0) * it }
+            .sum()
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // Or read a large test input from the `data/Day01_test.txt` file:
+    val testLines = readInput("Day01_test")
+    val (testLeft, testRight) = testLines.map { line ->
+        line.split("""\s+""".toRegex()).let { it[0].toInt() to it[1].toInt()}
+    }.unzip()
+    check(part1(testLeft, testRight) == 11)
+    check(part2(testLeft, testRight) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
+    // Read the input from the `data/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val (left, right) = input.map { line ->
+        line.split("""\s+""".toRegex()).let { it[0].toInt() to it[1].toInt()}
+    }.unzip()
+    part1(left, right).println()
+    part2(left, right).println()
 }
